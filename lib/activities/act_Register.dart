9ef25c7e1f9747629bd974972ea69d29e904b85a
Juan Diego <juandiegoperezf@hotmail.com>
2022-06-18 10:34:34 -0500

    components of login & register

--- lib/activities/act_Register.dart
+++ lib/activities/act_Register.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../components/comp_inputText.dart';
+import '../components/comp_buttonLogin.dart';
 
 class register extends StatefulWidget {
   @override
@@ -6,125 +8,56 @@ class register extends StatefulWidget {
 }
 
 class _registerState extends State<register> {
+  TextEditingController name = new TextEditingController();
+  TextEditingController phone = new TextEditingController();
   TextEditingController email = new TextEditingController();
   TextEditingController password = new TextEditingController();
 
+  final _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        margin: EdgeInsets.only(top: 0, left: 20, right: 20, bottom: 50),
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Image.asset('../assets/music.jpg', height: 200), //cambia el nombre de tu imagen
+    return Form(
+      key: _formkey,
+      child: Column(
+          children: <Widget>[
             Container(
-                margin: EdgeInsets.fromLTRB(50,30,50,0),
-                padding: EdgeInsets.fromLTRB(50,0,50,0),
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(245, 245, 245, 1),
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: TextName()
-            ),
-            Container(
-                margin: EdgeInsets.fromLTRB(50,25,50,25),
-                padding: EdgeInsets.fromLTRB(50,0,50,0),
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(245, 245, 245, 1),
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: TextPhone()
-            ),
-            Container(
-                margin: EdgeInsets.fromLTRB(50,0,50,25),
-                padding: EdgeInsets.fromLTRB(50,0,50,0),
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(245, 245, 245, 1),
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: TextEmail()
-            ),
-            Container(
-                margin: EdgeInsets.fromLTRB(50,0,50,0),
-                padding: EdgeInsets.fromLTRB(50,0,50,0),
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(245, 245, 245, 1),
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: TextPassword()
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 70),
-              decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.circular(50),
+              margin: EdgeInsets.only(top: 50, left: 20, right: 20, bottom: 50),
+              child: Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('../assets/music.jpg', height: 200),
+                        inputText(
+                            controller: name,
+                            placeholder: "Nombre",
+                            voidMessage: "Ingrese su nombre"
+                        ),
+                        inputText(
+                            controller: phone,
+                            placeholder: "Celular",
+                            voidMessage: "Ingrese su número de celular"
+                        ),
+                        inputText(
+                            controller: email,
+                            placeholder: "Correo",
+                            voidMessage: "Ingrese su email"
+                        ),
+                        inputText(
+                            controller: password,
+                            placeholder: "Contraseña",
+                            voidMessage: "Ingrese su contraseña"
+                        ),
+                        buttonLogin(
+                            formkey: _formkey,
+                            name: "Registrarse"
+                        ),
+                      ]
+                  )
               ),
-              padding: EdgeInsets.fromLTRB(60,0,60,0),
-              child: Button(),
             ),
-            SizedBox(
-              height: 50,
-            ),
-            Text("¿Nuevo usuario? Crea una cuenta !")
-          ]),
-        ),
+          ]
       ),
-    );
-  }
-
-  Widget TextName() {
-    return TextField(
-      controller: email,
-      decoration: InputDecoration(
-        hintText: "Nombre",
-      ),
-
-    );
-  }
-
-  Widget TextPhone() {
-    return TextField(
-      controller: email,
-      decoration: InputDecoration(
-        hintText: "Celular",
-      ),
-
-    );
-  }
-
-  Widget TextEmail() {
-    return TextField(
-      controller: email,
-      decoration: InputDecoration(
-        hintText: "Correo",
-      ),
-
-    );
-  }
-
-  Widget TextPassword(){
-    return TextField(
-      controller: password,
-      obscureText: true,
-      decoration: InputDecoration(
-          hintText: "Contraseña"
-      ),
-    );
-  }
-
-
-  Widget Button(){
-    return FlatButton(
-      child: Text(
-        'Iniciar sesión',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 20,
-          fontFamily: 'Montserrat',
-        ),
-      ),
-      onPressed: () {},
     );
   }
 }
