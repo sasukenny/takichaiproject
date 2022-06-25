aa9689110ae515c15fc1895bde5901b4f4ac16cd
Juan Diego <juandiegoperezf@hotmail.com>
2022-06-25 10:01:36 -0500

    logo register fixed

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,12 @@ class MyApp extends StatelessWidget {
         )
       ),
       // home: const NowPlayingPage(title: 'Takichai'),
-      home: const Home(title: 'Takichai'),
-      //home: const Wrapper(title: 'Now Playing'),
+      // home: const Home(title: 'Takichai'),
+      // home: const Wrapper(title: 'Now Playing'),
       //home: const NowPlayingPage(title: 'Now Playing'),
       // home: const MusicList(title: 'Music List'),
       //home: const AboutUs(title: "About Us"),
-      //home: const register(),
+      home: const register(),
 
     );
   }
